7a63daed59d06dc3bcd5628be976c141a8216522
Jordan2266 <54198889+Jordan2266@users.noreply.github.com>
2020-03-10 07:55:49 -0400

    Update Capital.js

--- Capital.js
+++ Capital.js
@@ -1,1 +1,13 @@
+function button(){
+    var inputArray = [];
+  var size = 5; 
+  
+  for(var i=0; i<size; i++) {
+    
+    inputArray[i] = prompt('Enter Element ' + (i+1));
+    inputArray.sort();
+    document.getElementById("demo").innerHTML = inputArray.map(function(x){ return x.toUpperCase() });
+  }
+  var str = String(inputArray).toUpperCase().split(",");
+  }
 
